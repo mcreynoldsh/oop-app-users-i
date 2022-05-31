5555135693561a94f60516604e765fd7c25a270d
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-31 16:51:30 -0500

    initial

--- User.py
+++ User.py
@@ -1,1 +1,13 @@
-# your User class goes here
+class User:
+    def __init__(self, name, email_address, license_number):
+        self.name= name
+        self.email_address= email_address
+        self.license_number= license_number
+
+
+
+person = User("Tyler","tyler29@gmail.com",2345603)
+
+person2 = User("Jane","janejane@gmail.com",3049585)
+print( person.name)
+print(person2.name)
